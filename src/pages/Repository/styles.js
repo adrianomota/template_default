50d8d67842d787dfa4b528178b62a1715d1077dc
Adriano Mota <adrianowsh@hotmail.com>
2019-07-16 00:33:04 -0300

    globalstyle

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    color: #fff;
+    font-size: 48px;
+  }
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  html, body, #root {
+     height: 100%;
+  }
+  body {
+    background: #7159c1;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  button {
+      cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
